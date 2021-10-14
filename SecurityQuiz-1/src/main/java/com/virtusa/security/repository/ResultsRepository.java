085a12b93efb95afc141eb45771fc4a2201072fa
AdabalaDivya <67156454+AdabalaDivya@users.noreply.github.com>
2021-10-14 16:43:47 +0530

    Add files via upload

---
+++ SecurityQuiz-1/src/main/java/com/virtusa/security/repository/ResultsRepository.java
@@ -0,0 +1,9 @@
+package com.virtusa.security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.virtusa.security.model.Results;
+
+public interface ResultsRepository extends JpaRepository<Results, Integer> {
+
+}
